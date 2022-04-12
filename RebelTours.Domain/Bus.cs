9a6419253d7d4e8c0facfa39f56fb9c19edbce14
ümit balıkçı <u.balikci@hotmail.com>
2022-04-12 12:12:13 +0300

    added busClass

---
+++ RebelTours.Domain/Bus.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RebelTours.Domain
+{
+    public class Bus
+    {
+        public Bus(int id, int busModelId, string registrationPlate, short year, SeatingType seatMapping, int distanceTraveled)
+        {
+            Id = id;
+            BusModelId = busModelId;
+            RegistrationPlate = registrationPlate;
+            Year = year;
+            SeatMapping = seatMapping;
+            DistanceTraveled = distanceTraveled;
+        }
+        public int Id { get; set; }
+        public int BusModelId { get; }
+        public string RegistrationPlate { get; }
+        public short Year { get;}
+        public SeatingType SeatMapping { get; set; }
+        public int SeatCount {
+            get 
+            {
+                if (BusModel.Type == BusType.Coach)
+                {
+                    if (BusModel.SeatCapacity == 52)
+                    {
+                        if (SeatMapping == SeatingType.Standard)
+                        {
+                            return 52;
+                        }
+                        else
+                        {
+                            return SeatMapping == SeatingType.Deluxe ? 38 : 26;
+                        }
+
+                    }
+                    else if (BusModel.SeatCapacity == 48)
+                    {
+                        if (SeatMapping == SeatingType.Standard)
+                        {
+                            return 48;
+                        }
+                        else
+                        {
+                            return SeatMapping == SeatingType.Deluxe ? 35 : 24;
+                        }
+                    }
+                    else
+                    {
+                        if (SeatMapping == SeatingType.Standard)
+                        {
+                            return 44;
+                        }
+                        else
+                        {
+                            return SeatMapping == SeatingType.Deluxe ? 32 : 22;
+                        }
+                    }
+                }
+                else
+                {
+                    if (BusModel.SeatCapacity == 30)
+                    {
+                        if (SeatMapping == SeatingType.Standard)
+                        {
+                            return 30;
+                        }
+                        else
+                        {
+                            return SeatMapping == SeatingType.Deluxe ? 20 : 15;
+                        }
+
+                    }
+                    else if (BusModel.SeatCapacity == 28)
+                    {
+                        if (SeatMapping == SeatingType.Standard)
+                        {
+                            return 28;
+                        }
+                        else
+                        {
+                            return SeatMapping == SeatingType.Deluxe ? 19 : 14;
+                        }
+                    }
+                    else
+                    {
+                        if (SeatMapping == SeatingType.Standard)
+                        {
+                            return 26;
+                        }
+                        else
+                        {
+                            return SeatMapping == SeatingType.Deluxe ? 18 : 13;
+                        }
+                    }
+                }
+            }}
+        public int DistanceTraveled{ get; set; }
+
+        public BusModel BusModel { get; set; }
+    }
+}
---
+++ RebelTours.Persistence/Configurations/BusConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RebelTours.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RebelTours.Persistence.Configurations
+{
+    public class BusConfiguration : IEntityTypeConfiguration<Bus>
+    {
+        public void Configure(EntityTypeBuilder<Bus> builder)
+        {
+            builder.HasKey(bu => bu.Id);
+            builder.HasOne(bu => bu.BusModel)
+                .WithMany()
+                .HasForeignKey(bu => bu.BusModelId);
+
+            builder.Property(bu => bu.RegistrationPlate)
+                .HasColumnType("varchar(50)");
+
+            builder.Property(bu => bu.Year)
+                .HasColumnType("int");
+            builder.Property(bu => bu.SeatMapping)
+                .IsRequired();
+            builder.Property(bu => bu.SeatCount)
+                .HasColumnType("int");
+            builder.Property(bu => bu.DistanceTraveled)
+                .HasColumnType("int");
+        }
+    }
+}
